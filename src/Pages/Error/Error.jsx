25c98f2dcbb9fb4d851e76a8466afe0a3c286349
Dhaya3132 <dhayalanu103@gmail.com>
2024-08-22 18:58:30 +0530

    Feat - Error Page

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Error = () => {
   return (
@@ -6,6 +7,9 @@ const Error = () => {
         <h2 className='text-8xl font-semibold'>404</h2>
         <p className='text-5xl font-medium'>Not found</p>
         <p className='font-light text-xl'>Please check your route</p>
+        <div className='mt-10'>
+          <Link to='/'><button className='bg-blue-50 border-2 border-blue-200 text-black font-normal px-5 py-2 rounded-3xl'>Back to home</button></Link>
+        </div>
     </div>
   )
 }
